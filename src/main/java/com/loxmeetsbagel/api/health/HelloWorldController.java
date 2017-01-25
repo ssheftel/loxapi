097329acfe044c11d6f4a056ce2cc39d68771cc1
Sam Sheftel <mathias@mailinator.com>
2017-01-24 19:38:14 -0600

    add endpoint

---
+++ src/main/java/com/loxmeetsbagel/api/health/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.loxmeetsbagel.api.health;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}

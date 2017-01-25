87a4cb04a2aece8c461248d1e019074adc6e02c0
Sam Sheftel <mathias@mailinator.com>
2017-01-24 18:51:07 -0600

    first commit

---
+++ src/main/java/com/loxmeetsbagel/api/LoxapiApplication.java
@@ -0,0 +1,12 @@
+package com.loxmeetsbagel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoxapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoxapiApplication.class, args);
+	}
+}

82b25c572244c2dacdf19f643d19aa0d9b2a4945
Sam Sheftel <mathias@mailinator.com>
2017-01-26 22:12:27 -0600

    working auth

--- src/main/java/com/loxmeetsbagel/api/security/auth/ajax/AjaxAwareAuthenticationFailureHandler.java
+++ src/main/java/com/loxmeetsbagel/api/security/auth/ajax/AjaxAwareAuthenticationFailureHandler.java
@@ -11,12 +11,14 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class AjaxAwareAuthenticationFailureHandler implements AuthenticationFailureHandler {
     private final ObjectMapper mapper;
 
